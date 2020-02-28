619a246bfc23b11a387b8772d8b3c3b245e79b88
Yuliya <cisan@ukr.net>
2020-02-29 00:44:44 +0200

    Change application_controller.rb and routes.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def hello
-  	render text: "hola, mundo!"
+  def goodbye
+  	render text: "goodbye, world!"
   end
 end
